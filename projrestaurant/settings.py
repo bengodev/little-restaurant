086f904bef25d71686ef6d925474f411c7cecee1
bengodev <rahadshz@gmail.com>
2025-08-01 16:26:57 +0600

    Add Whitenoise to use staticfiles with websocket settings

--- projrestaurant/settings.py
+++ projrestaurant/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # After SecurityMiddleware
 ]
 
 ROOT_URLCONF = 'projrestaurant.urls'
